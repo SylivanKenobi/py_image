a80bc26dc39b08be7eacd8646c180bf0854c5039
Sylivan Kenobi <sypsilon@hotmail.com>
2021-03-11 22:29:18 +0100

    make shading percentual by darkest shade

--- ascii_printer.py
+++ ascii_printer.py
@@ -3,53 +3,33 @@ import os
 
 # Get Terminal size
 rows, columns = os.popen('stty size', 'r').read().split()
-im = Image.open("assets/2021_0226_20372800.jpg")
+im = Image.open("assets/5733121373_044ed8cb27_o.jpg")
 # Convert image to grayscale
 im = im.convert('L')
-im = im.transpose(Image.ROTATE_180)
+im = im.transpose(Image.ROTATE_90)
 width, height = im.size
-im = im.resize((int(width/2), int(height/2)), Image.ANTIALIAS)
+im = im.resize((int(width/4), int(height/4)), Image.ANTIALIAS)
+# im = im.resize((int(rows), int(columns)), Image.ANTIALIAS)
 width, height = im.size
 # im.show()
 f = open("ascii_image.txt", "w+")
+max = max(list(im.getdata()))
+grayscale = "@%#*+=-:."
+bla = len(grayscale)
+# for w in range(width):
+#     print("")
+#     for h in range(height):
+#         gray = im.getpixel((w,h))
+#         one = ((100 / max) * gray) / 100
+#         two = bla * one
+#         print(grayscale[int(two) - 1], end='')   
 
 for w in range(width):
     f.write("\n")
     for h in range(height):
         gray = im.getpixel((w,h))
-        # print(gray)
-        if (gray < 40):
-            f.write("#")
-        elif (gray >= 40 and gray < 80):
-            f.write("P")
-        elif (gray >= 80 and gray < 120):
-            f.write("O")
-        elif (gray >= 120 and gray < 160): 
-            f.write("I")
-        elif (gray >= 160 and gray < 200):
-            f.write(";")
-        elif (gray >= 200 and gray < 240):
-            f.write(":")
-        else:
-            f.write(".")
+        one = ((100 / max) * gray) / 100
+        two = bla * one
+        f.write(grayscale[int(two) - 1])
 
-# for w in range(width):
-#     print("")
-#     for h in range(height):
-#         gray = im.getpixel((w,h))
-#         # print(gray)
-#         if (gray < 10):
-#             print("#", end='')
-#         elif (gray >= 20 and gray < 50):
-#             print("P", end='')
-#         elif (gray >= 50 and gray < 80):
-#             print("O", end='')
-#         elif (gray >= 80 and gray < 110): 
-#             print("I", end='')
-#         elif (gray >= 110 and gray < 140):
-#             print(";", end='')
-#         elif (gray >= 140 and gray < 240):
-#             print(":", end='')
-#         else:
-#             print(".", end='')
         
